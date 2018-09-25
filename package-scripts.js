3d2ce6a949c2ffadba0a841d7c5922a6529b8544
Rafa Mel <melrcm@gmx.net>
2018-09-25 02:54:39 +0200

    Fixes need for /lib in partial imports

--- package-scripts.js
+++ package-scripts.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const path = require('path');
 const registerSx = (sx, _ = (global.SX = {})) =>
   Object.keys(sx).forEach((key) => (global.SX[key] = sx[key]));
 const sx = (name) => `node -r ./package-scripts.js -e "global.SX.${name}()"`;
@@ -12,8 +14,11 @@ module.exports = scripts({
     'nps validate',
     exit0('shx rm -r lib'),
     'shx mkdir lib',
-    'babel src --out-dir lib'
+    'babel src --out-dir lib',
+    'shx cp LICENSE ./lib',
+    sx('package')
   ]),
+  publish: 'nps build && cd ./lib && npm publish',
   watch: 'onchange "./src/**/*.{js,jsx,ts}" -i -- nps private.watch',
   fix: `prettier --write "./**/*.{js,jsx,ts,json,scss}"`,
   lint: {
@@ -30,7 +35,7 @@ module.exports = scripts({
   clean: `${exit0('shx rm -r lib coverage')} && shx rm -rf node_modules`,
   // Private
   private: {
-    watch: `${sx('clear')} && nps lint`,
+    watch: `${sx('clear')} && nps lint && babel src --out-dir lib`,
     test_watch: `${sx('clear')} && nps test`,
     validate_last: `npm outdated || ${sx('countdown')}`
   }
@@ -45,5 +50,17 @@ registerSx({
       process.stdout.write('\r' + process.env.MSG + ' ' + i);
       !i-- && (clearInterval(t) || true) && console.log('\n');
     }, 1000);
+  },
+  package: () => {
+    const plain = fs.readFileSync(path.join(__dirname, 'package.json'));
+    const package = JSON.parse(plain);
+
+    package.main = './index.js';
+    delete package.scripts.prepublishOnly;
+
+    fs.writeFileSync(
+      path.join(__dirname, 'lib/package.json'),
+      JSON.stringify(package, null, 2)
+    );
   }
 });
