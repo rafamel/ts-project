adb86907169d712e5c047c8d03654ce607b15860
Rafa Mel <melrcm@gmx.net>
2019-06-21 14:57:19 +0200

    build(babel): adds dynamic import support

--- .babelrc.js
+++ .babelrc.js
@@ -19,7 +19,10 @@ module.exports = {
     ['babel-plugin-module-resolver', { alias: { '~': './src' } }],
     ['@babel/plugin-proposal-decorators', false],
     ['@babel/plugin-proposal-object-rest-spread', { useBuiltIns: true }],
-    '@babel/plugin-proposal-class-properties'
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-syntax-dynamic-import',
+    '@babel/plugin-syntax-import-meta',
+    vars.node && 'dynamic-import-node'
   ].filter(Boolean),
   ignore: ['node_modules', '**/*.d.ts']
 };
