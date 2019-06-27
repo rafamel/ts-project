92091416f7cde3e36670013fa281eaf91551b2c3
Rafa Mel <melrcm@gmx.net>
2019-06-27 18:13:08 +0200

    feat(templates/react): adds Home page and segments

--- templates/react/src/App.tsx
+++ templates/react/src/App.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import TopBar from './segments/TopBar';
+import Footer from '~/segments/Footer';
+import Home from './pages/Home';
 import { variable, mui } from './theme';
 import { injectGlobal } from 'emotion';
 import LoadFadeIn from './components/LoadFadeIn';
@@ -18,7 +21,11 @@ function App(): JSX.Element {
       <MuiThemeProvider theme={mui}>
         <LoadingBar active={store.loading.isLoading} />
         <LoadFadeIn onLoad={store.loading.ends}>
-          <div>React starter</div>
+          <>
+            <TopBar />
+            <Home />
+            <Footer />
+          </>
         </LoadFadeIn>
       </MuiThemeProvider>
     </StylesProvider>
---
+++ templates/react/src/segments/TopBar.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function TopBar(): JSX.Element {
+  return (
+    <menu>
+      <div>Topbar</div>
+    </menu>
+  );
+}
