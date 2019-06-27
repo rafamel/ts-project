daf3eb8c407cd89c8a36f015807ec8c60935e488
Rafa Mel <melrcm@gmx.net>
2019-06-27 18:03:47 +0200

    feat(templates/react): adds LoadFadeIn component

--- templates/react/src/App.tsx
+++ templates/react/src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { variable, mui } from './theme';
 import { injectGlobal } from 'emotion';
+import LoadFadeIn from './components/LoadFadeIn';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import { StylesProvider } from '@material-ui/styles';
+import store from './store';
 import { observer } from 'mobx-react';
 
 injectGlobal({ ':root': variable.styles });
@@ -13,7 +15,9 @@ function App(): JSX.Element {
   return (
     <StylesProvider injectFirst>
       <MuiThemeProvider theme={mui}>
-        <div>React starter</div>
+        <LoadFadeIn onLoad={store.loading.ends}>
+          <div>React starter</div>
+        </LoadFadeIn>
       </MuiThemeProvider>
     </StylesProvider>
   );
