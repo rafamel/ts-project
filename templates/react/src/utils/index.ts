d90250ec3a146318afcbc5230ddfdb04b56cfc09
Rafa Mel <melrcm@gmx.net>
2019-06-30 06:34:13 +0200

    feat(templates/react): adds useStyles util

--- templates/react/src/utils/index.ts
+++ templates/react/src/utils/index.ts
@@ -2,3 +2,4 @@ export { default as logger } from './logger';
 export { default as media } from './media';
 export { default as palette } from './palette';
 export { default as sw } from './sw';
+export { default as useStyles } from './use-styles';
