add2fef6d7421ec40c9a7110befa4c124d663fa7
Rafa Mel <melrcm@gmx.net>
2019-06-30 06:35:35 +0200

    feat(templates/react): adds caprover deploy script

--- templates/react/kpo.scripts.js
+++ templates/react/kpo.scripts.js
@@ -1,3 +1,12 @@
+const kpo = require('kpo');
 const { scripts } = require('./project.config');
 
-module.exports.scripts = scripts;
+module.exports.scripts = {
+  ...scripts,
+  'deploy:caprover': [
+    kpo.ensure`deploy`,
+    kpo.line`tarjs -cvf ./deploy/deploy.tar --portable
+      ./captain-definition ./build/*`,
+    'caprover deploy -t ./deploy/deploy.tar'
+  ]
+};
