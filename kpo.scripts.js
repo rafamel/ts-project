d7b25c502b40523f921f6c7f0af810643aff6e4a
Rafa Mel <melrcm@gmx.net>
2019-06-30 05:50:23 +0200

    chore: uses riseup for setup

---
+++ kpo.scripts.js
@@ -0,0 +1,34 @@
+const { kpo, remove, series } = require('kpo');
+const { scripts } = require('./project.config');
+
+module.exports.scripts = {
+  ...scripts,
+  bootstrap: [
+    series('npm i', { cwd: './packages' }),
+    'lerna bootstrap',
+    kpo`build`
+  ],
+  link: 'lerna link',
+  build: [
+    kpo`@common build`,
+    kpo`@monorepo build`,
+    kpo`@tooling build`,
+    kpo`@library build`,
+    kpo`@react build`
+  ],
+  'clean:modules': function() {
+    return [
+      remove('./packages/node_modules', { confirm: true }),
+      scripts['clean:modules'].bind(this)
+    ];
+  },
+  preversion: kpo`:stream lint`,
+  validate: function() {
+    return [
+      scripts.validate.bind(this),
+      series('npm outdated', { cwd: './packages', silent: true })
+    ];
+  },
+  /* Hooks */
+  postinstall: kpo`bootstrap`
+};
--- packages/library/project.config.js
+++ packages/library/project.config.js
@@ -1,25 +1,58 @@
 const path = require('path');
-const { default: slim } = require('slimconf');
+const { default: create } = require('@riseup/library');
+const pkg = require('./package.json');
 
-module.exports = slim({
-  // Transpiles to esnext
-  esnext: false,
+module.exports = create({
+  // Whether it is a monorepo child project
+  monorepo: true,
   // Enables typescript and declaration files
   typescript: true,
-  // Extensions allowed for each file type, as a comma separated string
-  ext: {
-    js: 'js,cjs,mjs,jsx',
-    ts: 'ts,tsx'
-  },
   // Paths used on build
   paths: {
     root: __dirname,
-    docs: path.join(__dirname, 'docs')
+    docs: path.join(__dirname, '../../docs/', pkg.name),
+    build: path.join(__dirname, 'pkg')
   },
-  release: {
+  // Source code aliases
+  assign: {
+    alias: { '~': './src' },
+    todo: ['xxx', 'fixme', 'todo', 'refactor']
+  },
+  version: {
     // Build project on version bump. Boolean.
     build: true,
     // Generate docs from TS on version bump. Boolean.
-    docs: true
+    docs: false
+  },
+  extend: {
+    babel: {
+      strategy: 'deep',
+      configure: {}
+    },
+    eslint: {
+      strategy: 'deep',
+      configure: {
+        overrides: [
+          {
+            files: ['*'],
+            rules: {
+              // 'rule-name': 0
+            }
+          }
+        ]
+      }
+    },
+    jest: {
+      strategy: 'deep',
+      configure: {
+        modulePathIgnorePatterns: []
+      }
+    },
+    typedoc: {
+      strategy: 'deep',
+      configure: {
+        exclude: ['**/__mocks__/**/*']
+      }
+    }
   }
 });
