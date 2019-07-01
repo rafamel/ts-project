4128431c267e630db2efb6d1ae1143e003d69eb3
Rafa Mel <melrcm@gmx.net>
2019-07-02 01:51:26 +0200

    feat(templates/react): adds useScript hook to utils

--- templates/react/src/utils/index.ts
+++ templates/react/src/utils/index.ts
@@ -2,5 +2,7 @@ export { default as logger } from './logger';
 export { default as media } from './media';
 export { default as palette } from './palette';
 export { default as sw } from './sw';
+/* Hooks */
+export { default as useScript } from 'react-script-hook';
 export { default as useStyles } from './use-styles';
 export * from './contextual';
