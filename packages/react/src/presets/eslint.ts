6c6ff739ed503bf2f7bd5d1111e1a64ac0a443ed
Rafa Mel <melrcm@gmx.net>
2019-07-02 02:48:17 +0200

    fix(packages/react): fixes eslint presets typings

--- packages/react/src/presets/eslint.ts
+++ packages/react/src/presets/eslint.ts
@@ -1,3 +1,4 @@
+import path from 'path';
 import { IOfType, IOptionsReact } from '~/types';
 import { getEslint as getToolingEslint } from '@riseup/tooling';
 import { DeepRequired } from 'utility-types';
@@ -6,7 +7,10 @@ import { merge } from 'merge-strategies';
 export default function getEslint(
   opts: DeepRequired<IOptionsReact>
 ): IOfType<any> {
-  const tooling = getToolingEslint(opts);
+  const tooling = getToolingEslint({
+    ...opts,
+    paths: { ...opts.paths, build: path.join(opts.paths.root, 'build') }
+  });
 
   return merge(tooling, {
     extends: [...tooling.extends, require.resolve('eslint-config-react-app')],
