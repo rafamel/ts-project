05adfc90e767e4c027fd5b119500ade33b10d8d1
Rafa Mel <melrcm@gmx.net>
2019-09-11 11:59:45 +0200

    feat(templates/react): removes caprover deploy script

--- templates/react/kpo.scripts.js
+++ templates/react/kpo.scripts.js
@@ -1,12 +1,1 @@
-const kpo = require('kpo');
-const { scripts } = require('./project.config');
-
-module.exports.scripts = {
-  ...scripts,
-  'deploy:caprover': [
-    kpo.ensure`deploy`,
-    kpo.line`tarjs -cvf ./deploy/deploy.tar --portable
-      ./captain-definition ./build/*`,
-    'caprover deploy -t ./deploy/deploy.tar'
-  ]
-};
+module.exports.scripts = require('./project.config').scripts;
