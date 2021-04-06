17ac5932c8019bcedd8789f8e381d3d99e4a5774
Rafa Mel <melrcm@gmx.net>
2021-04-06 14:54:47 +0200

    feat(packages/cli): adds cli export and riseup bin

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,3 +1,13 @@
-export default function main(): null {
-  return null;
+import { loadPackage } from 'cli-belt';
+import { bin } from 'kpo';
+
+export async function cli(): Promise<void> {
+  const pkg = await loadPackage(__dirname, { title: false });
+
+  return bin({
+    bin: 'riseup',
+    file: 'riseup.config.js',
+    description: pkg.description || '',
+    version: pkg.version || 'Unknown'
+  });
 }
