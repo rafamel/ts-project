42974b49c14d9fb24671a087d6cf1e0f0ea99b9b
Rafa Mel <melrcm@gmx.net>
2021-04-10 22:24:03 +0200

    chore(packages): add monorepo package

---
+++ packages/monorepo/README.md
@@ -0,0 +1,9 @@
+# @riseup/monorepo
+
+[![Version](https://img.shields.io/npm/v/@riseup/monorepo.svg)](https://www.npmjs.com/package/@riseup/monorepo) [![Build Status](https://img.shields.io/travis/rafamel/riseup/master.svg)](https://travis-ci.org/rafamel/riseup) [![Coverage](https://img.shields.io/coveralls/rafamel/riseup/master.svg)](https://coveralls.io/github/rafamel/riseup) [![Dependencies](https://img.shields.io/david/rafamel/riseup.svg?path=packages%2Fmonorepo)](https://david-dm.org/rafamel/riseup?path=packages%2Fmonorepo) [![Vulnerabilities](https://img.shields.io/snyk/vulnerabilities/npm/@riseup/monorepo.svg)](https://snyk.io/test/npm/@riseup/monorepo) [![License](https://img.shields.io/github/license/rafamel/riseup.svg)](https://github.com/rafamel/riseup/blob/master/LICENSE) [![Types](https://img.shields.io/npm/types/@riseup/monorepo.svg)](https://www.npmjs.com/package/@riseup/monorepo)
+
+> _Riseup_ tasks and configurations for monorepos.
+
+## Install
+
+[`npm install @riseup/monorepo`](https://www.npmjs.com/package/@riseup/monorepo)
---
+++ packages/monorepo/kpo.tasks.js
@@ -0,0 +1,126 @@
+const lint = require('../scripts/tasks/lint');
+const test = require('../scripts/tasks/test');
+const build = require('../scripts/tasks/build');
+const eslint = require('../scripts/configure/eslint');
+const jest = require('../scripts/configure/jest');
+const pika = require('../scripts/configure/pika');
+const babel = require('../scripts/configure/babel');
+const typescript = require('../scripts/configure/typescript');
+
+module.exports = {
+  lint: lint.bind(null, {
+    args: {
+      paths: ['./src', './test']
+    },
+    options: {
+      paths: {
+        root: process.cwd()
+      },
+      extensions: {
+        js: ['js', 'jsx'],
+        ts: ['ts', 'tsx']
+      }
+    },
+    config: {
+      eslint: eslint({
+        args: {
+          prettier: true,
+          todo: ['fixme', 'todo', 'refactor']
+        },
+        options: {
+          extensions: {
+            js: ['js', 'jsx'],
+            ts: ['ts', 'tsx']
+          },
+          paths: {
+            root: process.cwd(),
+            alias: {}
+          }
+        }
+      })
+    }
+  }),
+  build: build.bind(null, {
+    options: {
+      paths: {
+        root: process.cwd(),
+        dest: './pkg'
+      }
+    },
+    config: {
+      pika: pika({
+        args: {
+          nodeOnly: true,
+          assets: []
+        },
+        options: {
+          paths: {
+            root: process.cwd()
+          },
+          extensions: {
+            js: ['js', 'jsx'],
+            ts: ['ts', 'tsx']
+          }
+        },
+        config: {
+          babel: {
+            node: babel({
+              args: {
+                targets: { node: '12.0.0' }
+              },
+              options: {
+                alias: {}
+              }
+            })
+          },
+          typescript: typescript({
+            options: {
+              paths: {
+                root: process.cwd()
+              }
+            }
+          })
+        }
+      }),
+      babel: {
+        standard: babel({
+          args: {
+            targets: { esmodules: true }
+          },
+          options: {
+            alias: {}
+          }
+        })
+      }
+    }
+  }),
+  test: test.bind(null, {
+    options: {
+      paths: {
+        root: process.cwd()
+      }
+    },
+    config: {
+      jest: jest({
+        options: {
+          extensions: {
+            js: ['js', 'jsx'],
+            ts: ['ts', 'tsx']
+          }
+        },
+        config: {
+          babel: {
+            node: babel({
+              args: {
+                targets: { node: '12.0.0' }
+              },
+              options: {
+                alias: {}
+              }
+            })
+          }
+        }
+      })
+    }
+  })
+};
