faffc1d8640ad08269681fa328f47dc1e56329e4
Rafa Mel <melrcm@gmx.net>
2021-04-27 21:07:48 +0200

    feat(packages/react): add reconfigureEslintReact

--- packages/react/src/paths.ts
+++ packages/react/src/paths.ts
@@ -1,5 +1,8 @@
 export const paths = {
   babel: {
     presetReactApp: require.resolve('babel-preset-react-app')
+  },
+  eslint: {
+    configReactApp: require.resolve('eslint-config-react-app')
   }
 };
